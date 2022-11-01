d60acdcee0919d106a9a6786f5ae73d3e8bab45e
Михаил С. Голозубов <golozubovms@rubezh.ru>
2022-11-01 16:58:43 +0400

    Добавил чтение ключей из консоли, добавил сборку пакетов из исходников

--- install_deb.sh
+++ install_deb.sh
@@ -1,5 +1,9 @@
-#Create deb source files in bash-0.1 folder. bashdb_0.1.orig.tar.xz and bashdb_0.1-1_amd64.build is creating upper folder.
+#Create deb source files in bash-0.1 folder. bashdb_0.1.orig.tar.xz is creating upper bashdb-0.1 folder.
 #dh_make --indep --createorig
-#Build deb package from deb source files
-#sudo debuild -us -uc
+#Build deb package from deb source file
+echo "---------Building deb package from source files---------"
+cd ./bashdb-0.1
+dpkg-buildpackage -rsudo
+cd ../
+echo "---------Installing builded deb package---------"
 sudo apt install ./bashdb_0.1-1_all.deb
